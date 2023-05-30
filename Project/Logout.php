51379c9ccc1f0d91c9cd607c5ddfd252a993162d
LinkNexus <116998582+LinkNexus@users.noreply.github.com>
2023-05-30 14:05:37 +0100

    Delete Project directory

--- Project/Logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-session_start();
-setcookie('remember', NULL, -1);
-unset($_SESSION['user_infos']);
-$_SESSION['flash']['success'] = 'You are now disconnected';
-header('Location: Login.php');
